e38dfbf0f1f179bfdc27937bb3fde434c564021a
chrischongyj <39231238+chrischongyj@users.noreply.github.com>
2021-06-27 17:19:30 +0800

    Initial discuss details sreen

--- src/components/AvatarIcon.tsx
+++ src/components/AvatarIcon.tsx
@@ -1,6 +1,7 @@
 import { theme } from "constants/theme";
 import { getInitials } from "lib/helpers";
 import React from "react";
+<<<<<<< HEAD
 import { ImageSourcePropType, ImageURISource } from "react-native";
 import { Avatar, Box, Image } from "react-native-magnus";
 import { BodyTextRegular, TinyText } from "./StyledText";
@@ -8,15 +9,17 @@ import { BodyTextRegular, TinyText } from "./StyledText";
 interface AvatarIcon {
   uri?: string;
   label?: string;
+  radius?: number;
+  px?: number;
 }
 
-const AvatarIcon: React.FC<AvatarIcon> = ({ uri, label }) => {
+const AvatarIcon: React.FC<AvatarIcon> = ({ uri, label , radius=50, px=5}) => {
   return (
     <Box>
       {uri ? (
         <Image
-          h={50}
-          w={50}
+          h={radius}
+          w={radius}
           rounded="circle"
           borderWidth={2}
           borderColor="white"
--- src/screens/Plan/DiscussDetailsScreen.tsx
+++ src/screens/Plan/DiscussDetailsScreen.tsx
@@ -1,16 +1,152 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { useWindowDimensions, TouchableOpacity, Platform } from "react-native";
 import Container from "components/Container";
 import { Subheading, SmallText } from "components/StyledText";
 import { RouteProp, useRoute } from "@react-navigation/core";
 import { AppStackParamList } from "types/types";
+import { Text, Div, Button, Icon, Input } from "react-native-magnus"
+import AvatarIcon from "components/AvatarIcon";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import RBSheet from "react-native-raw-bottom-sheet";
 
 const DiscussDetailsScreen = () => {
   const route = useRoute<RouteProp<AppStackParamList, "DiscussDetails">>();
   const { meetingInfo, participants, pendingParticipants } = route.params;
+  const windowWidth = useWindowDimensions().width;
+  const windowHeight = useWindowDimensions().height;
+  const fontSize = 30;
+  const iconPadding = 10;
+  var heartColour = "#F78826";
+  const divPadding = 20;
+  
+
+  const refRBSheet = useRef();
+
+  const openModal = () => {
+    refRBSheet.current.open()
+  }
+
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
+
+
+
+
   return (
     <Container avoidHeader>
+<<<<<<< HEAD
       <Subheading>Plan Screen</Subheading>
       <SmallText>{JSON.stringify(meetingInfo, null, 2)}</SmallText>
+=======
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
+      <RBSheet
+        ref={refRBSheet}
+        closeOnDragDown={false}
+        closeOnPressMask={true}
+        height={windowHeight * 0.15}
+        customStyles={{
+          wrapper: {
+            backgroundColor: "transparent"
+          },
+          draggableIcon: {
+            backgroundColor: "#000"
+          }
+        }}
+      >
+        <Div row justifyContent="center" alignItems="center">
+          <Button  bg="lightgrey" my={iconPadding} >
+            <Text fontSize={fontSize}>Make Co-Organiser</Text>
+          </Button>
+        </Div>
+      </RBSheet>
+      <Div row pb={divPadding}>
+        <Text fontSize={fontSize}>Party-cipants🎊</Text>
+        <TouchableOpacity>
+          <Icon name="pluscircleo" fontSize={fontSize} color="black" pl={iconPadding}/>
+        </TouchableOpacity>
+      </Div>
+      
+      <Div row pb={divPadding}>
+        <TouchableOpacity onLongPress={openModal}>
+          <AvatarIcon radius={70} label="Chris" source={{uri: "https://picsum.photos/200"}}/>
+        </TouchableOpacity>
+        <TouchableOpacity onLongPress={openModal}>
+          <AvatarIcon radius={70} label="Chris" source={{uri: "https://picsum.photos/200"}}/>
+        </TouchableOpacity>
+        <TouchableOpacity onLongPress={openModal}>
+          <AvatarIcon radius={70} label="Chris" source={{uri: "https://picsum.photos/200"}}/>
+        </TouchableOpacity>
+      </Div>
+
+      <Div row pb={divPadding}>
+        <Text fontSize={fontSize}>When should we meet?</Text>
+        <Icon name="calendar" fontSize={fontSize} color="black" pl={iconPadding}/>
+      </Div>
+
+      <Div row alignItems="center" pb={divPadding}>
+        <Input onFocus={showDatepicker} placeholder="Add a date!" w={windowWidth * 0.4} mr={iconPadding} fontSize={fontSize * 0.75} value={date.toString()}/>
+        <Input onFocus={showTimepicker} placeholder="Start" w={windowWidth * 0.2} mr={iconPadding} fontSize={fontSize * 0.75}/>
+        <Text mr={iconPadding}>-</Text>
+        <Input onFocus={showTimepicker} placeholder="End" w={windowWidth * 0.2} fontSize={fontSize * 0.75}/>
+      </Div>
+
+      <Div row alignItems="center" pb={divPadding}>
+        <Text fontSize={fontSize}>Now what should we do...</Text>
+        <TouchableOpacity>
+        <Icon name="dice-4-outline" fontFamily="MaterialCommunityIcons" fontSize={fontSize+5} color="black" pl={iconPadding}/>
+        </TouchableOpacity>
+      </Div>
+
+      <Div row mb={divPadding}>
+          <Text fontSize={fontSize} color={heartColour}>12</Text>
+          <TouchableOpacity>
+            <Icon name="heart" fontSize={fontSize} color={heartColour} px={iconPadding}/>
+          </TouchableOpacity>
+          <Text fontSize={fontSize} bg="#FBC99D" rounded={10} px={iconPadding}>I'm kim?</Text>
+      </Div>
+
+      <Div row alignItems="center">
+        <TouchableOpacity>
+          <Icon name="pluscircleo" color="black" fontSize={fontSize} px={iconPadding}/>
+        </TouchableOpacity>
+        <Input placeholder="Add a suggestion!" w={windowWidth * 0.7} fontSize={fontSize * 0.75} />
+      </Div>
+
+      <Div mt="auto" mb={divPadding} >
+        <Button bg="#F47D15" rounded="circle" w={windowWidth * 0.9} h={windowHeight * 0.07}>
+          <Text fontSize={fontSize} color="white">CONFIRM</Text>
+        </Button>
+      </Div>
+>>>>>>> dc9788b (Initial discuss details sreen)
     </Container>
   );
 };
